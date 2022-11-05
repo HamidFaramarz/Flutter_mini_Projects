0c553e1ba9a2947d10a81281c405c42ef39ecf48
Hamid Faramarz <101446076+HamidFaramarz@users.noreply.github.com>
2022-11-05 18:21:01 +0430

    Create actionAppBar.dart

---
+++ action/actionAppBar.dart
@@ -0,0 +1,10 @@
+AppBar(
+        title: Text("InkWell"),
+        actions: <Widget>[
+          IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.shopping_cart),
+            tooltip: 'you pressed the button',
+          )
+        ],
+      ),
